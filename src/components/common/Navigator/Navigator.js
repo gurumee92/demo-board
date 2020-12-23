804fd32b312f205ea410ddf0e2ac97537956d46b
gurumee <gurumee92@gmail.com>
2020-12-23 13:52:01 +0900

    ADD react hook in Navigator

--- src/components/common/Navigator/Navigator.js
+++ src/components/common/Navigator/Navigator.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 export default function Navigator() {
+    const [isAuth, setAuth] = useState(false);
+
     return (
         <div className="navigator">
-            <span><Link to="/posts/create">글쓰기</Link></span>
-            <span><Link to="/">로그아웃</Link></span>
+            { 
+                (isAuth) ? (
+                    <>
+                    <span><Link to="/posts/create">글쓰기</Link></span>
+                    <span onClick={() => setAuth(false)}><Link to="/">로그아웃</Link></span>
+                    </>
+                ) : (
+                    <span onClick={() => setAuth(true)}>로그인</span>
+                )
+            }
         </div>
     )
 }
