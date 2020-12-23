15ade92de649c1869a4b0a703e43f64dd41ad735
gurumee <gurumee92@gmail.com>
2020-12-23 17:29:39 +0900

    UPDATE Post

--- src/components/post/Post/Post.js
+++ src/components/post/Post/Post.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function PostItem({id, title, content, author, createdAt, updatedAt}) {
+export default function Post({id, title, content, author, createdAt, updatedAt, isOwner}) {
     return (
         <div className="post">
             <h3 className="post__title">{title}</h3>
             <div className="post__extra_info">
-                <div className="post__extra_info__links">
-                    <Link to={`/posts/update/${id}`}><span>수정</span></Link>
-                    <Link to="/"><span>삭제</span></Link>
-                </div>
+                {
+                    (isOwner) ? (
+                        <div className="post__extra_info__links">
+                            <Link to={`/posts/update/${id}`}><span>수정</span></Link>
+                            <Link to="/"><span>삭제</span></Link>
+                        </div>
+                    ) : (
+                        <></>
+                    )
+                }
                 <span>{author}</span>
                 <span>{createdAt}</span>
                 <span>{updatedAt}</span>
