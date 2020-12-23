21717e87c37052ffe2254883b27144211912acb2
gurumee <gurumee92@gmail.com>
2020-12-23 15:10:06 +0900

    ADD LoginModal

---
+++ src/components/common/LoginModal/LoginModal.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+export default function LoginModal({ setAuth, setModalUp }) {
+    const [isSignUp, setSignUp] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if (isSignUp) {
+            setSignUp(!isSignUp);
+            return;
+        }
+
+        setAuth(true);
+        setModalUp(false);
+    };
+    return (
+        <div className="login__modal" onSubmit={(e) => handleSubmit(e)}>
+            <form>
+                <input name="username" type="text"/>
+                <input name="password" type="password"/>
+                {
+                    (isSignUp) ? (
+                        <>
+                        <input name="password_check" type="password"/>
+                        <span onClick={() => setSignUp(!isSignUp)}>to login</span>
+                        </>
+                    ) : (
+                        <>
+                        <span onClick={() => setSignUp(!isSignUp)}>to signup</span>
+                        </>
+                    )
+                }
+                
+                <button type="submit">submit</button>
+            </form>
+        </div>
+    )
+}
--- src/components/common/Navigator/Navigator.js
+++ src/components/common/Navigator/Navigator.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
+import LoginModal from "../LoginModal";
+
 export default function Navigator() {
     const [isAuth, setAuth] = useState(false);
-
+    const [isModalUp, setModalUp] = useState(false);
     return (
         <div className="navigator">
             { 
                 (isAuth) ? (
                     <>
                     <span><Link to="/posts/create">글쓰기</Link></span>
-                    <span onClick={() => setAuth(false)}><Link to="/">로그아웃</Link></span>
+                    <span onClick={() => setAuth(!isAuth)}><Link to="/">로그아웃</Link></span>
                     </>
                 ) : (
-                    <span onClick={() => setAuth(true)}>로그인</span>
+                    <span onClick={() => setModalUp(!isModalUp)}>로그인</span>
+                )
+            }
+
+            {
+                (isModalUp) ? (
+                    <LoginModal setAuth={setAuth} setModalUp={setModalUp} />
+                ) : (
+                    <></>
                 )
             }
         </div>
