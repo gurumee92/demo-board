9bf9e48b2242f0ee48b34ea2a5f3e31196870ae5
gurumee <gurumee92@gmail.com>
2020-12-30 14:43:24 +0900

    #ADD PostItem.css

--- src/components/post/PostItem/PostItem.js
+++ src/components/post/PostItem/PostItem.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import "./PostItem.css";
+
 export default function PostItem({id, title, content, author, createdAt}) {
+    const collapsed = (content.length > 250) ? `${content.slice(0, 250)}...` : content;
     return (
-        <Link to={`/posts/${id}`}>
             <div className="post__item">
-                <h3 className="post__item__title">{title}</h3>
-                <p className="post__item__content">{content}</p>
+                <Link to={`/posts/${id}`}>
+                <h1 className="post__item__title">{title}</h1>
+                <p className="post__item__content">{collapsed}</p>
                 <div className="post__item__extra_info">
                     <span>{author}</span>
                     <span>{createdAt}</span>
                 </div>
+                </Link>
             </div>
-        </Link>
     )
 }
