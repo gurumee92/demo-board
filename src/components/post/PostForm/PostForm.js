3db3e788307b0427e082ac0e5403c1e1481fe420
gurumee <gurumee92@gmail.com>
2020-12-30 16:41:33 +0900

    #ADD PostForm.css

--- src/components/post/PostForm/PostForm.js
+++ src/components/post/PostForm/PostForm.js
@@ -1,6 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import "./PostForm.css";
 
 export default function PostForm({initTitle, initContent, onSubmit}) {
+    const history = useHistory();
     const [title, setTitle] = useState(initTitle);
     const [content, setContent] = useState(initContent);
     const  [error, setError] = useState("");
@@ -20,14 +24,28 @@ export default function PostForm({initTitle, initContent, onSubmit}) {
         onSubmit(title, content);
     };
 
+    const onClickCancel = () => {
+        history.goBack();
+        return <></>
+    }
+
     return (
-        <div>
+        <div className="post__form__wrapper">
             <form className="post__form" onSubmit={onSubmitForm}>
-                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>        
-                <textarea value={content} onChange={(e) => setContent(e.target.value)}></textarea>
-                <button type="submit">submit</button>
+                <label for="title">제목</label>
+                <input name="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>        
+
+                <label for="content">내용</label>
+                <textarea name="content" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+
+                <div className="post__form__button__area">
+                    <button type="submit">submit</button>
+                    <button onClick={onClickCancel}>cancel</button>
+                </div>
             </form>
-            {error}
+            <div className="post__form__error__area">
+                { error } 
+            </div>
         </div>
     )
 }
