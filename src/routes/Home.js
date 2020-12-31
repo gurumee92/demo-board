222a43776723054b4b5f97a88a9a4b72b239f8d4
gurumee <gurumee92@gmail.com>
2020-12-31 11:10:44 +0900

    #ADD search-bar

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import React, { useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 import { Link } from 'react-router-dom';
 
 import PostItem from 'components/post/PostItem';
-import { postListState }  from 'stores/posts';
 import { accountSelector } from 'stores/accounts';
-import { getPostList } from 'apis/posts';
+import { getPostList, searchPosts } from 'apis/posts';
 
 import "./Home.css";
 
 export default function Home() {
-    const [postList, setPostList] = useRecoilState(postListState);
+    const [searchText, setSearchText] = useState("");
+    const [error, setError] = useState("");
+    const [postList, setPostList] = useState([]);
     const account = useRecoilValue(accountSelector);
     const isAuth = (account.username !== "" && account.access_token !== "");
 
@@ -28,6 +29,24 @@ export default function Home() {
         return () => setPostList([]);
     }, [setPostList]);
 
+    const onSubmit = async () => {
+        if (searchText === "") {
+            setError("검색어를 입력하세요.");
+            return;
+        }
+
+        const { data } = await searchPosts(searchText);
+        setPostList(data.map(p => ({
+            ...p,
+            author: p.owner_name,
+            createdAt: p.created_at,
+            updatedAt: p.updatedAt,
+        })));
+
+        setSearchText("");
+        setError("");
+    }
+
     return (
         <div className="home">
             {
@@ -37,15 +56,26 @@ export default function Home() {
                     </Link>    
                 )
             }
+            <div className="home__search">
+                <input type="text" placeholder="검색어를 입력하세요." value={searchText} onChange={e => setSearchText(e.target.value)}/>
+                <button onClick={onSubmit}>search</button>
+                <div className="home__search__error">
+                    {error}
+                </div>
+            </div>
             <div className="home__post__list">
             {
-                postList.map((p) => <PostItem key={p.id}
-                    id={p.id}
-                    title={p.title}
-                    content={p.content}
-                    author={p.author}
-                    createdAt={p.createdAt}
-                    />)
+                (postList.length > 0) ? (
+                    postList.map((p) => <PostItem key={p.id}
+                                                id={p.id}
+                                                title={p.title}
+                                                content={p.content}
+                                                author={p.author}
+                                                createdAt={p.createdAt}
+                                                />)
+                ) : (
+                    <div className="home__post__list__empty__result">검색 결과가 없습니다.</div>
+                )
             }
             </div>
         </div>
